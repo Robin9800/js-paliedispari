800003d4b452de20346cb7be0eac0786e48c210b
Robin9800 <robin.costanzo@gmail.com>
2022-03-16 01:36:52 +0100

    while content

--- script.js
+++ script.js
@@ -35,3 +35,19 @@ function invertWord(parola){
     
     return inverted;
 }
+
+/*Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.*/
+
+let pariODispari = prompt('pari o dispari?');
+
+/*Finchè l'utente non inserisce effettivamente qualcosa e finchè ciò che viene inseirito
+non è uguale a "pari" o "dispari" chiedi all'utente di inserire qualcosa*/
+
+while(!pariODispari || (pariODispari !== 'pari' % pariODispari !== 'dispari')){
+    pariODispari = prompt('pari o dispari?');
+}
