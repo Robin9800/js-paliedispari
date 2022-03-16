61307cbc0ad776b510a20e836a7bf3ff7b87d37a
Robin9800 <robin.costanzo@gmail.com>
2022-03-16 01:24:34 +0100

    "if" content

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ Creare una funzione per capire se la parola inserita è palindroma*/
 //Chiedi all'utente di inserire una parola
 let word = prompt('Inserisci una parola');
 
+
+
 /*Finchè il valore non è una parola oppure la lunghezza è uguale a 0 richiedi di inserire la parola.*/
 while(!word || word.length === 0){
     word = prompt('Inserisci una parola');
@@ -13,6 +15,13 @@ while(!word || word.length === 0){
 //Inverti la parola
 const invertedWord = invertWord(word);
 console.log(invertedWord);
+
+/*Crea una funzione per capire se la parola inserita è palindroma*/
+if(word===invertedWord){
+    console.log('è palindroma');
+}else{
+        console.log('non è palindroma');
+}
 
 //Creo un ciclo che va da 0 alla lunghezza in caratteri della stringa
 function invertWord(parola){
